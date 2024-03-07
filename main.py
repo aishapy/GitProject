e809bbed6b63cf1f90a2ae553b3f8c43a4257dc4
Aisha Animashaun <155480525+aishapy@users.noreply.github.com>
2024-03-07 13:47:43 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -22,8 +22,15 @@ def calculation(x, y, operator):
     num1 = float(input("please enter a nuber: "))
     num2 = float(input("please enter a nuber: "))
 
+ops = ['+', '-', '*', '/']
+
+while True:
     operator = input("Enter the math symbol you would like to use:")
-
-    calc = calculation(num1, num2, operator)
-
-    print(calc)
+    
+    if operator not in ops:
+        print('''Your selected operator is  not valid.
+            Please use one of the following symbols:
+            +(addition), -(subtraction), x(multiplication), /(division)''')
+        
+    else:
+        print ("That's right")
